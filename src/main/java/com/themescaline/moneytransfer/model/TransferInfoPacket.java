bcc76464f451a2a7a8c19a06ba8bcc4209a1278f
themescaline <lex.korovin@gmail.com>
2019-10-11 10:32:16 +0300

    change TransferDataPacket field names

--- src/main/java/com/themescaline/moneytransfer/model/TransferInfoPacket.java
+++ src/main/java/com/themescaline/moneytransfer/model/TransferInfoPacket.java
@@ -11,7 +11,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @AllArgsConstructor
 @XmlRootElement
 public class TransferInfoPacket {
-    Long fromAccount;
-    Long toAccount;
+    Long fromAccountId;
+    Long toAccountId;
     Double amount;
 }
--- src/main/java/com/themescaline/moneytransfer/service/TransferServiceImpl.java
+++ src/main/java/com/themescaline/moneytransfer/service/TransferServiceImpl.java
@@ -20,7 +20,7 @@ public class TransferServiceImpl implements TransferService {
 
     @Override
     public boolean doTransfer(TransferInfoPacket packet) {
-        log.info(MessageFormat.format("Transferring {0} from account ID {1} to account ID {2}", packet.getFromAccount(), packet.getToAccount(), packet.getAmount()));
-        return accountDAO.doTransfer(packet.getFromAccount(), packet.getToAccount(), packet.getAmount());
+        log.info(MessageFormat.format("Transferring {0} from account ID {1} to account ID {2}", packet.getFromAccountId(), packet.getToAccountId(), packet.getAmount()));
+        return accountDAO.doTransfer(packet.getFromAccountId(), packet.getToAccountId(), packet.getAmount());
     }
 }
